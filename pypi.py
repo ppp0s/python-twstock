0bd9865c3db4335baec4b76e41d19bfa872115b2
ppp0s <97409880+ppp0s@users.noreply.github.com>
2022-09-06 06:49:15 +0800

    Update pypi.py

--- pypi.py
+++ pypi.py
@@ -1,3 +1,28 @@
-https://pypi.org/project/twstock/
+from typing import Type
+import requests
+import bs4
+import pandas as pd
 
-https://hackmd.io/@s02260441/HJcMcnds8
+url ="https://fubon-ebrokerdj.fbs.com.tw/Z/ZG/ZGK_DD.djhtm"
+
+res = requests.get(url)
+soup = bs4.BeautifulSoup(res.text,"html.parser")
+print(soup.prettify())  #輸出排版後HTML
+#print(soup.title.string) #投信買賣超
+
+#data = soup.select('table')[1]
+
+##使用read_html建立DataFrame
+#df = pd.read_html(data.prettify())
+#dfs = df[0]
+
+#刪減不要的row(index)
+#dfs = dfs.drop(0)
+
+#刪減不要的欄(index)
+#dfs = dfs.drop(columns=[2,3,4,5, 6, 7, 8, 9], axis=1)
+
+#s = dfs
+
+#ds_reset = s.set_index(0)
+#print(ds_reset)
