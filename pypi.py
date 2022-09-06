441a2002545fddfc16ff07b06652a10f42d603de
ppp0s <97409880+ppp0s@users.noreply.github.com>
2022-09-07 00:17:23 +0800

    Update pypi.py

--- pypi.py
+++ pypi.py
@@ -7,25 +7,25 @@ import bs4
 import time
 import pandas as pd
 
-headers = { #處理防爬蟲機制 (User-agent)
+#處理防爬蟲機制 (User-agent)
+headers = { 
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.75 Safari/537.36 Edge/17.17134',
 }
 
-url ="https://fubon-ebrokerdj.fbs.com.tw/Z/ZG/ZGK_DD.djhtm" #投信買賣超頁面
+url ="https://fubon-ebrokerdj.fbs.com.tw/Z/ZG/ZGK_DD.djhtm"
 
 res = requests.get(url,headers=headers)
 soup = bs4.BeautifulSoup(res.text,"lxml")
 
 f = open("d:\\output.txt", "w",encoding='utf-8') #開啟檔案並指定編碼類型
-
 f.write(str(soup))  #寫入*.txt檔案
 f.close()           #關閉檔案
+
+
 #print(soup.prettify())  #輸出排版後HTML
-
-
 #print(soup.title.string) #投信買賣超
-
 #print(soup.find_all(value='005'))
+
 #for x in soup.find_all('option'):
 #    print(x, end = "\n")
 
